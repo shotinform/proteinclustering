7dc6caf054d4e2dcab9f57934924f081f1a91ed0
Marko <marko.nikitovic01@gmail.com>
2024-05-23 14:34:37 +0200

    change in enrichgo_analysis

--- enrich_go_analysis.R
+++ enrich_go_analysis.R
@@ -1,85 +1,127 @@
-# Load required packages
 library(clusterProfiler)
 library(org.Hs.eg.db)
 library(ReactomePA)
 library(pheatmap)
+library(grid)
+library(gridExtra)
 
-# test exmaple
-geneList <- c("TP53", "EGFR", "MYC", "BRAF", "PTEN")
+generate_heatmaps <- function(geneList, module_name, output_directory) {
+  entrez_ids <- bitr(geneList, fromType="SYMBOL", toType="ENTREZID", OrgDb=org.Hs.eg.db)
+  
+  # Gene Ontology Enrichment Analysis for Biological Process (BP)
+  ego_bp <- enrichGO(gene=geneList,
+                     OrgDb=org.Hs.eg.db,
+                     keyType="SYMBOL",
+                     ont="BP",  # Biological Process
+                     pAdjustMethod="BH",  # Benjamini-Hochberg correction
+                     pvalueCutoff=0.1)
+  
+  # Gene Ontology Enrichment Analysis for Molecular Function (MF)
+  ego_mf <- enrichGO(gene=geneList,
+                     OrgDb=org.Hs.eg.db,
+                     keyType="SYMBOL",
+                     ont="MF", # Molecular function
+                     pAdjustMethod="BH",
+                     pvalueCutoff=0.1)
+  
+  ego_bp <- as.data.frame(ego_bp)
+  ego_mf <- as.data.frame(ego_mf)
+  
+  ego_bp$genes <- sapply(ego_bp$geneID, function(x) paste(unique(unlist(strsplit(x, "/"))), collapse = ", "))
+  ego_mf$genes <- sapply(ego_mf$geneID, function(x) paste(unique(unlist(strsplit(x, "/"))), collapse = ", "))
+  
+  ego_bp <- ego_bp[order(ego_bp$pvalue), ]
+  ego_bp <- ego_bp[1:10, ]
+  
+  ego_mf <- ego_mf[order(ego_mf$pvalue), ]
+  ego_mf <- ego_mf[1:10, ]
+  
+  matrix_bp <- matrix(ego_bp$pvalue, nrow = nrow(ego_bp), ncol = 1, dimnames = list(ego_bp$ID, NULL))
+  matrix_mf <- matrix(ego_mf$pvalue, nrow = nrow(ego_mf), ncol = 1, dimnames = list(ego_mf$ID, NULL))
+  
+  # Save BP heatmap
+  png(file.path(output_directory, paste0(module_name, "_biological_processes_heatmap.png")), width = 1920, height = 1080)
+  heatmap <- pheatmap(matrix_bp, 
+                      color = colorRampPalette(c("red", "white"))(10),
+                      main = module_name,
+                      cluster_rows = FALSE,  
+                      cluster_cols = FALSE,
+                      border_color = "black",
+                      fontsize_row = 12,  
+                      fontsize_col = 12,
+                      silent = TRUE,
+                      cellwidth = 300,  
+                      legend = FALSE)  
+  
+  annotation_texts_left <- ego_bp$Description
+  annotation_texts_right <- ego_bp$genes
+  
+  grid.newpage()
+  grid.draw(heatmap$gtable)
+  
+  y_coords <- seq(from = 0.9, to = 0.05, length.out = nrow(matrix_bp))
+  
+  for (i in 1:length(annotation_texts_left)) {
+    grid.text(annotation_texts_left[i], 
+              x = unit(0.35, "npc"), 
+              y = unit(y_coords[i], "npc"),
+              just = "right",
+              gp = gpar(fontsize = 12))
+  }
+  
+  for (i in 1:length(annotation_texts_right)) {
+    grid.text(annotation_texts_right[i], 
+              x = unit(0.65, "npc"), 
+              y = unit(y_coords[i], "npc"),
+              just = "left",
+              gp = gpar(fontsize = 12))
+  }
+  dev.off()  
+  
+  # Save MF heatmap
+  png(file.path(output_directory, paste0(module_name, "_molecular_function_heatmap.png")), width = 1920, height = 1080)
+  heatmap <- pheatmap(matrix_mf, 
+                      color = colorRampPalette(c("brown", "white"))(10),
+                      main = module_name,
+                      cluster_rows = FALSE,
+                      cluster_cols = FALSE,
+                      border_color = "black",
+                      fontsize_row = 12,
+                      fontsize_col = 12,
+                      silent = TRUE,
+                      cellwidth = 300,
+                      legend = FALSE)  
+  
+  annotation_texts_left_mf <- ego_mf$Description
+  annotation_texts_right_mf <- ego_mf$genes
+  
+  grid.newpage()
+  grid.draw(heatmap$gtable)
+  
+  for (i in 1:length(annotation_texts_left_mf)) {
+    grid.text(annotation_texts_left_mf[i], 
+              x = unit(0.35, "npc"), 
+              y = unit(y_coords[i], "npc"),
+              just = "right",
+              gp = gpar(fontsize = 12))
+  }
+  
+  for (i in 1:length(annotation_texts_right_mf)) {
+    grid.text(annotation_texts_right_mf[i], 
+              x = unit(0.65, "npc"), 
+              y = unit(y_coords[i], "npc"),
+              just = "left",
+              gp = gpar(fontsize = 12))
+  }
+  dev.off()  
+}
 
-entrez_ids <- bitr(geneList, fromType="SYMBOL", toType="ENTREZID", OrgDb=org.Hs.eg.db)
-
-ego_bp <- enrichGO(gene=geneList,
-                OrgDb=org.Hs.eg.db,
-                keyType="SYMBOL",
-                ont="BP",  # Biological Process
-                pAdjustMethod="BH",  # Benjamini-Hochberg correction
-                pvalueCutoff=0.1)
-#                qvalueCutoff=0.1)
-
-ego_mf <- enrichGO(gene          = geneList,
-                   OrgDb         = org.Hs.eg.db,
-                   keyType       = "SYMBOL",
-                   ont           = "MF",
-                   pAdjustMethod = "BH",
-                   pvalueCutoff  = 0.1)
-#                   qvalueCutoff = 0.1)
-
-ego_bp <- as.data.frame(ego_bp)
-ego_mf <- as.data.frame(ego_mf)
-
-# Add gene names to the data frames
-ego_bp$genes <- sapply(ego_bp$geneID, function(x) paste(unique(unlist(strsplit(x, "/"))), collapse = ", "))
-ego_mf$genes <- sapply(ego_mf$geneID, function(x) paste(unique(unlist(strsplit(x, "/"))), collapse = ", "))
-
-## 
-ego_bp <- ego_bp[order(ego_bp$pvalue), ]  # sort by p-value
-ego_bp <- ego_bp[1:10, ]  # select top 10
-
-matrix_bp <- matrix(ego_bp$pvalue, nrow = nrow(ego_bp), ncol = 1, dimnames = list(ego_bp$ID, NULL))
-
-matrix_bp
-
-ego_bp$combined = paste(ego_bp$Description, ego_bp$genes, sep=": ")
-annotation_rows <- data.frame(
-  Description = ego_bp$combined
-)
-
-annotation_rows
-row.names(annotation_rows) <- ego_bp$ID
-annotation_rows
-
-
-pheatmap(matrix_bp, 
-         annotation_row = annotation_rows,
-         color = colorRampPalette(c("red", "white"))(10),
-         main = "Biological Processes (BP)",
-         cluster_rows = FALSE,  # Disable clustering if rows are already ordered by significance
-         cluster_cols = FALSE,
-         border_color = "black")
-
-# molecular function part
-ego_mf <- ego_mf[order(ego_mf$pvalue), ]  # sort by p-value
-ego_mf <- ego_mf[1:10, ]  # select top 10
-
-matrix_mf <- matrix(ego_mf$pvalue, nrow = nrow(ego_mf), ncol = 1, dimnames = list(ego_mf$ID, NULL))
-
-matrix_mf
-
-ego_mf$combined = paste(ego_mf$Description, ego_mf$genes, sep=": ")
-annotation_rows <- data.frame(
-  Description = ego_mf$combined
-)
-
-annotation_rows
-row.names(annotation_rows) <- ego_mf$ID
-annotation_rows
-
-
-pheatmap(matrix_mf, 
-         annotation_row = annotation_rows,
-         color = colorRampPalette(c("brown", "white"))(10),
-         main = "Molecular Function (MF)",
-         cluster_rows = FALSE,  
-         cluster_cols = FALSE,
-         border_color = "black")
+input_lines <- readLines("files/enrich_analysis/modules.txt")
+i <- 1
+while (i <= length(input_lines)) {
+  module_name <- trimws(input_lines[i])
+  i <- i + 1
+  gene_list <- unlist(strsplit(trimws(input_lines[i]), " "))
+  i <- i + 1
+  generate_heatmaps(gene_list, module_name, "files/enrich_analysis/enrich_go/")
+}
